89e10dfd5099a37aada5a11c6d362014d627bb6e
alewerner <xandewerner.2@gmail.com>
2019-03-28 20:30:27 -0300

    code pos review

--- media.py
+++ media.py
@@ -2,16 +2,31 @@ import webbrowser
 
 
 class Movie():
-    """ Documentation: This class provides a way to store movie related information  """
+    """
+    Documentation: This class provides a way to store movie related
+    information and execute a youtube trailer, if the movie had it
+    """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    # 'eh para indicar que eh uma palavra reservada'
-    # 'self pode ser trocada, mas eh uma'
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """
+        This are the parameters of the constructor
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube: string
+        """
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailler(self):
+        """
+        this function calls the trailer_youtube from the movie
+        and reproduces it on the web browser
+        :return: a youtube video
+        """
         webbrowser.open(self.trailer_youtube_url)
